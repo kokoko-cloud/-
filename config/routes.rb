eaa644aa816c4cc90dad1f3a239eae4b216f3213
”名前” <>
2020-11-18 14:14:32 +0000

    1

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'pages/help'
+  get 'pages/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+  get 'pages/help'
+  root 'pages#index'
+end
+
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/help'
+end
