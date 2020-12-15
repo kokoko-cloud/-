190b473429460b3f83b2172999570a4cedf4883b
”名前” <>
2020-12-15 11:47:22 +0000

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,37 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
   get 'pages/help'
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
   get 'pages/help'
   root 'pages#index'
 end
 
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
   root 'pages#index'
   get 'pages/help'
 end
+
+Rails.application.routes.draw do
+  get 'sessions/new'
+  
+  root 'pages#index'
+  get 'pages/help'
+  
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
+end
+
