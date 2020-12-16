345603fe80a689543933b81d07309a1ac975f74e
”名前” <>
2020-12-16 12:17:54 +0000

    11

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,19 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true
     
-    has_secure_password
+    validates :name,
+            presence: true,
+            length: { in: 1..15 }
     
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }}
+    
+    has_secure_password
+    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+    valedates :password, length: { in: 8..32 }
+    format: { with: VALID_PASSWORD_REGEX }
+      
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,16}+\z/.freeze
+    format: { with: VALID_PASSWORD_REGEX }
     has_many :topics
 end
